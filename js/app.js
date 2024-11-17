96174bee50e3a780d4807c0594804bde93a98513
Nouhaila-Nachat <nouhailanachat23@gmail.com>
2024-11-17 01:04:27 +0100

    modification sur la fonction ajouter des parametres ,ajouter les conditions

--- js/app.js
+++ js/app.js
@@ -1,54 +1,36 @@
-/**
- * @function showNotification
- * @description cette fonction consist a envoyer une notification a l'utilisateurs.
- * 
- * 
- * @returns {notification} -  la fonction retourne une notification.
- * 
- * @example
- * // Exemple d'utilisation
- * showNotification('Bienvenue', 'Au sein de notre site');
+    /**
+     * @function showNotification
+     * @description cette fonction consist a envoyer une notification a l'utilisateurs.
+     * @param{body} le contenu de notification
+     * @param {icon} l'imge afficher dans la notification 
+     * @returns {notification} -  la fonction retourne une notification.
+     * @example
+     * // Exemple d'utilisation
+     * showNotification('Bienvenue', 'Au sein de notre site');
 
- */
-(async () => {
-    // create and show the notification
-    const showNotification = () => {
-        try {
-            // create a new notification
-        const notification = new Notification('JavaScript Notification API', {
-            body: 'hhh',
-            icon: './img/js.png',
-        });
-       console.log(notification);
+    */
+   
         
-        // close the notification after 30 seconds
-        setTimeout(() => {
-            notification.close();
-        }, 30 * 1000);
-
-        // navigate to a URL when clicked
-        notification.addEventListener('click', () => {
-
-            window.open('https://www.cmc.com', '_blank');
-        });
-        } catch (error) {
-            console.error(error);
+        function showNotification(body, icon) {
+            try {
+                Notification.requestPermission().then(permission => {
+                    if (permission === "granted") {
+                        const notification = new Notification('JavaScript Notification API', {
+                            body: body, 
+                            icon: icon 
+                        });
+                        console.log(notification);
+                        setTimeout(() => {
+                            notification.close();
+                        }, 30 * 1000);
+                    } else {
+                        console.log("Permission de notification refusée.");
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+            }
         }
-    }
-
-    // check notification permission
-    document.addEventListener('DOMContentLoaded', async () => {
-         Notification.requestPermission().then(() => {
-             if('Notifications' in window){
-                 console.log('Notifications');
-             }else{
-                 console.log('Notification');
-             }
-            showNotification();
-         });
-    });
-
-
-
-
-})();
+        
+        
+        
