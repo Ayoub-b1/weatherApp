5feb699b59b0bd3ad24fc95d5e4dad17c2d09394
Ayoub-b1 <ayoub.bourhfella1@gmail.com>
2024-11-20 17:51:13 +0100

    merger with hajjaj branch to include global style

--- js/fonctions.js
+++ js/fonctions.js
@@ -31,44 +31,43 @@ export function afficherAlert(type, message) {
     setTimeout(() => {
         alert.classList.remove('show');
         alert.remove();
-    }, 5000); 
+    }, 5000);
 }
 
-    /**
-     * @function showNotification
-     * @description cette fonction consist a envoyer une notification a l'utilisateurs.
-     * @param{body} le contenu de notification
-     * @param {icon} l'imge afficher dans la notification 
-     * @param {title} le titre du notification
-     * @returns {notification} -  la fonction retourne une notification.
-     * @example
-     * // Exemple d'utilisation
-     * showNotification('Bienvenue', 'Au sein de notre site');
+/**
+ * @function showNotification
+ * @description cette fonction consist a envoyer une notification a l'utilisateurs.
+ * @param{body} le contenu de notification
+ * @param {icon} l'imge afficher dans la notification 
+ * @param {title} le titre du notification
+ * @returns {notification} -  la fonction retourne une notification.
+ * @example
+ * // Exemple d'utilisation
+ * showNotification('Bienvenue', 'Au sein de notre site');
 
-    */
-   
-        
-    function showNotification(title, body, icon) {
-        try {
-            Notification.requestPermission().then(permission => {
-                if (permission === "granted") {
-                    const notification = new Notification(title, {
-                        body: body, 
-                        icon: icon 
-                    });
-                    console.log(notification);
-                    setTimeout(() => {
-                        notification.close();
-                    }, 30 * 1000);
-                } else {
-                    console.log("Permission de notification refusée.");
-                }
-            });
-        } catch (error) {
-            console.error(error);
-        }
+*/
+
+
+function showNotification(title, body, icon) {
+    try {
+        Notification.requestPermission().then(permission => {
+            if (permission === "granted") {
+                const notification = new Notification(title, {
+                    body: body,
+                    icon: icon
+                });
+                console.log(notification);
+                setTimeout(() => {
+                    notification.close();
+                }, 30 * 1000);
+            } else {
+                console.log("Permission de notification refusée.");
+            }
+        });
+    } catch (error) {
+        console.error(error);
     }
-    
-            
-            
-            
+}
+
+
+
