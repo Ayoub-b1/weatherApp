73651497da56625e8899b605dcea12ec6e706d91
Curly-kali <bedounia14@gmail.com>
2024-11-20 14:39:43 +0100

    ;

--- js/fonctions.js
+++ js/fonctions.js
@@ -33,3 +33,22 @@ export function afficherAlert(type, message) {
         alert.remove();
     }, 5000); 
 }
+
+/** 
+ * @function Ville
+ * @description Récupere la valeur de la localisation d'un input et retourne sa valeur
+ * 
+ * @return {ville} - La valeur de la ville
+ * 
+ * @exemple
+ * //Retourne Rabat
+*/
+
+function Ville() {
+    let ville = document.getElementById("InputVille").value;
+
+    if(ville === ""){
+        //traitement de la condition
+    }
+    return ville
+}
